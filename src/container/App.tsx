6b05456f509d8bdcfb5854c88cdab4fa8f5a548c
adarshkunwar <hellboyadarsh18@gmail.com>
2023-06-17 21:05:06 +0545

    initial commit

---
+++ src/container/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewPrisoners from "../Pages/NewPrisoners"
+
+const App = () => {
+  return (
+    <div className="bg-red-300">
+      <NewPrisoners />
+    </div>
+  )
+}
+
+export default App
