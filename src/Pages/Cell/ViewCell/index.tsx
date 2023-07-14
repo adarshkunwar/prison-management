7bd496112f975df967eb5182648b339e7c172e50
adarshkunwar <hellboyadarsh18@gmail.com>
2023-07-14 19:40:05 +0545

    cell view

--- src/Pages/Cell/ViewCell/index.tsx
+++ src/Pages/Cell/ViewCell/index.tsx
@@ -1,27 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
 import { BsPencilSquare, BsTrash } from 'react-icons/bs';
+import axios from '../../../HOC/axios/axios';
 import ModalDanger from '../../../components/UI/ModalDanger';
 import Table from '../../../components/UI/Table';
 import TableHead from '../../../components/UI/TableHead';
 import Page from '../../../container/Page';
 
-const fields = [
-  { cellID: 1, blockID: 1, capacity: 4, occupancy: 2 },
-  { cellID: 2, blockID: 1, capacity: 4, occupancy: 3 },
-  { cellID: 3, blockID: 2, capacity: 2, occupancy: 1 },
-  { cellID: 4, blockID: 2, capacity: 2, occupancy: 1 },
-  { cellID: 5, blockID: 2, capacity: 2, occupancy: 1 },
-];
+// const fields = [
+//   { cellID: 1, blockID: 1, capacity: 4, occupancy: 2 },
+//   { cellID: 2, blockID: 1, capacity: 4, occupancy: 3 },
+//   { cellID: 3, blockID: 2, capacity: 2, occupancy: 1 },
+//   { cellID: 4, blockID: 2, capacity: 2, occupancy: 1 },
+//   { cellID: 5, blockID: 2, capacity: 2, occupancy: 1 },
+// ];
 
-const heading = ['Cell ID', 'Block ID', 'Capacity', 'Occupancy', 'Actions'];
+type fields = {
+  id: string;
+  cellName: string;
+  currentOccupancy: number;
+  capacity: number;
+  block: {
+    blockName: string;
+  };
+};
+
+const heading = ['Cell Name', 'Block Name', 'Capacity', 'Occupancy', 'Actions'];
 
 const title = 'Cell';
 
 const Index = () => {
   const [showDelete, setShowDelete] = useState(false);
-  const [showView, setShowView] = useState(false);
   const [turnOff, setTurnOff] = useState(true);
+  const [fields, setFields] = useState<fields[]>([]);
+
+  const getData = useCallback(() => {
+    try {
+      axios
+        .get('/cell')
+        .then((res) => {
+          console.log(res.data.result);
+          setFields(res.data.result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   const handleUpdate = () => {
     console.log('update');
@@ -42,13 +69,13 @@ const Index = () => {
   useEffect(() => {
     if (turnOff) {
       setShowDelete(false);
-      setShowView(false);
+      getData();
       const interval = setTimeout(() => {
         setTurnOff(false);
         return clearTimeout(interval);
       }, 500);
     }
-  }, [turnOff]);
+  }, [turnOff, getData]);
 
   const modal = (
     <div
@@ -74,37 +101,38 @@ const Index = () => {
         {showDelete ? modal : null}
         <TableHead title={title} />
         <Table heading={heading}>
-          {fields.map((val, i) => {
-            return (
-              <tr
-                key={i}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          {fields &&
+            fields.map((val, i) => {
+              return (
+                <tr
+                  key={i}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                 >
-                  {val.cellID}
-                </td>
-                <td className="px-6 py-4">{val.blockID}</td>
-                <td className="px-6 py-4">{val.capacity}</td>
-                <td className="px-6 py-4">{val.occupancy}</td>
-                <td className="px-6 py-4">
-                  <div className="flex items-center gap-2">
-                    <div className="text-lg" onClick={handleView}>
-                      <AiOutlineEye />
+                  <td
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {val.cellName}
+                  </td>
+                  <td className="px-6 py-4">{val.block.blockName}</td>
+                  <td className="px-6 py-4">{val.capacity}</td>
+                  <td className="px-6 py-4">{val.currentOccupancy}</td>
+                  <td className="px-6 py-4">
+                    <div className="flex items-center gap-2">
+                      <div className="text-lg" onClick={handleView}>
+                        <AiOutlineEye />
+                      </div>
+                      <div onClick={handleUpdate}>
+                        <BsPencilSquare />
+                      </div>
+                      <div onClick={() => setShowDelete(true)}>
+                        <BsTrash />
+                      </div>
                     </div>
-                    <div onClick={handleUpdate}>
-                      <BsPencilSquare />
-                    </div>
-                    <div onClick={() => setShowDelete(true)}>
-                      <BsTrash />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
+                  </td>
+                </tr>
+              );
+            })}
         </Table>
       </div>
     </Page>
